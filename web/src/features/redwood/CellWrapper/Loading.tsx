a6ac50a739ecb3b2084544961941c7b6a5c2e13d
facinick <facinick@macmiller.Dlink>
2023-02-26 22:52:14 +0530

    progress bar change

--- web/src/features/redwood/CellWrapper/Loading.tsx
+++ web/src/features/redwood/CellWrapper/Loading.tsx
@@ -3,10 +3,7 @@ interface ComponentProps {}
 export const CellLoading = (props: ComponentProps) => {
   return (
     <div className="alert alert-warning max-w-md shadow-lg">
-      <div>
-        <progress className="progress w-56"></progress>
-        <span>{`Loading...`}</span>
-      </div>
+      <progress className="progress progress-primary w-56"></progress>
     </div>
   )
 }
