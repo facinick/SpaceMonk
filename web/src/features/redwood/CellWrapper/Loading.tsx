b474f8c866a1fc915646cfe3074ddea6816f8590
facinick <facinick@macmiller.Dlink>
2023-02-27 01:52:34 +0530

    loading UI change

--- web/src/features/redwood/CellWrapper/Loading.tsx
+++ web/src/features/redwood/CellWrapper/Loading.tsx
@@ -2,7 +2,7 @@ interface ComponentProps {}
 
 export const CellLoading = (props: ComponentProps) => {
   return (
-    <div className="alert alert-warning max-w-md shadow-lg">
+    <div className="alert max-w-md shadow-lg">
       <progress className="progress progress-primary w-56"></progress>
     </div>
   )
