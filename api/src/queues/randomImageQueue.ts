12b2e1cdef8ebdb04c164e3fea00c6f4e3ec721b
facinick <facinick@gmail.com>
2024-06-01 19:28:49 +0530

    redis update

--- api/src/queues/randomImageQueue.ts
+++ api/src/queues/randomImageQueue.ts
@@ -1,8 +1,14 @@
 import Queue from 'bull';
 import { randomImageResolver } from 'src/services/unsplashes/unsplashes';
 
-const REDIS_URL = process.env.REDIS_URL
-const randomImageQueue = new Queue('randomImageQueue', REDIS_URL);
+const redisOptions = {
+  redis: {
+    host: process.env.UPSTASH_REDIS_REST_URL,
+    password: process.env.UPSTASH_REDIS_REST_TOKEN
+  }
+};
+
+const randomImageQueue = new Queue('randomImageQueue', redisOptions);
 
 randomImageQueue.process(async (job, done) => {
   const result = await randomImageResolver(job.data); // Call the randomImage resolver
